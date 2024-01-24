c58786b9b0e85619b94c2a3523886b5860bf35e6
Gaurav Yadav <gayadav2@cisco.com>
2024-01-24 17:09:47 +0530

    initial version of project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-wc-tool
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	countBytes := flag.Bool("c", false, "The number of bytes in the input file")
+	countLines := flag.Bool("l", false, "The number of lines in the input file")
+	countWords := flag.Bool("w", false, "The number of words in the input file")
+	countChars := flag.Bool("m", false, "The number of words in the input file")
+	flag.Parse()
+
+	var fileData []byte
+	if filename := flag.Arg(0); filename != "" {
+		f, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println("error opening the file: ", err)
+			os.Exit(1)
+		}
+		fileData = f
+	} else {
+		fmt.Println("Please give a filename")
+		return
+	}
+
+	if *countBytes {
+		fmt.Println(len(fileData))
+	} else if *countLines {
+		fmt.Println(bytes.Count(fileData, []byte("\n")), file)
+	} else if *countWords {
+		fmt.Println(len(bytes.Fields(fileData)))
+	} else if *countChars {
+		fmt.Println(len(bytes.Runes(fileData)))
+	}
+}
